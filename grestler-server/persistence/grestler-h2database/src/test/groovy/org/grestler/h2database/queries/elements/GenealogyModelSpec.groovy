ad7fb9e96493254e97d078c60f8813244dcfcd87
Martin Nordberg <martin.nordberg@verizon.net>
2015-01-23 08:03:58 -0500

    Expanded metamodel loader test coverage.

--- grestler-server/persistence/grestler-h2database/src/test/groovy/org/grestler/h2database/queries/elements/GenealogyModelSpec.groovy
+++ grestler-server/persistence/grestler-h2database/src/test/groovy/org/grestler/h2database/queries/elements/GenealogyModelSpec.groovy
@@ -7,6 +7,8 @@ package org.grestler.h2database.queries.elements
 
 import org.grestler.h2database.datasource.H2DataSource
 import org.grestler.h2database.queries.attributes.AttributeTypeLoader
+import org.grestler.metamodel.api.attributes.EAttributeOptionality
+import org.grestler.metamodel.api.attributes.EDataType
 import org.grestler.metamodel.impl.MetamodelRepository
 import org.grestler.metamodel.spi.IMetamodelRepositorySpi
 import spock.lang.Specification
@@ -49,7 +51,25 @@ class GenealogyModelSpec
         vtPerson.parentPackage == genealogyPkg;
         vtPerson.name == "Person";
         vtPerson.superType.get() == rootVertexType;
-        vtPerson.attributes.size() == 2;
+        vtPerson.attributes.size() == 5;
+        vtPerson.attributes[0].name == "Given Name";
+        vtPerson.attributes[0].parentVertexType == vtPerson;
+        vtPerson.attributes[0].optionality == EAttributeOptionality.OPTIONAL;
+        vtPerson.attributes[0].type.name == "Person Name";
+        vtPerson.attributes[0].type.dataType == EDataType.STRING;
+        vtPerson.attributes[1].name == "Surname";
+        vtPerson.attributes[1].optionality == EAttributeOptionality.REQUIRED;
+        vtPerson.attributes[1].type.name == "Person Name";
+        vtPerson.attributes[1].type.dataType == EDataType.STRING;
+        vtPerson.attributes[2].name == "Current Assets";
+        vtPerson.attributes[2].type.name == "Asset Value Dollars";
+        vtPerson.attributes[2].type.dataType == EDataType.FLOAT64;
+        vtPerson.attributes[3].name == "Lifetime Address Count";
+        vtPerson.attributes[3].type.name == "Address Count";
+        vtPerson.attributes[3].type.dataType == EDataType.INTEGER32;
+        vtPerson.attributes[4].name == "Person ID";
+        vtPerson.attributes[4].type.name == "UUID";
+        vtPerson.attributes[4].type.dataType == EDataType.UUID;
 
         // edge types
         def rootEdgeType = m.findEdgeTypeRoot().get();
@@ -59,6 +79,7 @@ class GenealogyModelSpec
         etHasFather.superType.get() == rootEdgeType;
         etHasFather.tailVertexType == vtPerson;
         etHasFather.headVertexType == vtPerson;
+        etHasFather.attributes.size() == 0;
 
         def etHasMother = m.findEdgeTypeById( UUID.fromString( "e4c4a703-a294-11e4-b20d-08002751500b" ) ).get();
         etHasMother.parentPackage == genealogyPkg;
@@ -66,6 +87,12 @@ class GenealogyModelSpec
         etHasMother.superType.get() == rootEdgeType;
         etHasMother.tailVertexType == vtPerson;
         etHasMother.headVertexType == vtPerson;
+        etHasMother.attributes.size() == 1;
+        etHasMother.attributes[0].name == "Calls Weekly";
+        etHasMother.attributes[0].parentEdgeType == etHasMother;
+        etHasMother.attributes[0].optionality == EAttributeOptionality.OPTIONAL;
+        etHasMother.attributes[0].type.name == "Calls Weekly";
+        etHasMother.attributes[0].type.dataType == EDataType.BOOLEAN;
 
     }
 
