b871ce06dd20da51f35931fab227aac2fe523f80
Martin Nordberg <martin.nordberg@verizon.net>
2016-11-17 19:39:01 -0500

    * Added crude test for vertex creation/update.

--- barlom-client/barlom-gdbconsole/webapp/customelements/vertextyperow.es6.js
+++ barlom-client/barlom-gdbconsole/webapp/customelements/vertextyperow.es6.js
@@ -9,11 +9,13 @@ class VertexTypeRow extends HTMLElement {
     let me = $( this );
 
     let deleteId = 'vertextypes-' + me.attr( 'uuid' ) + '-delete';
+    let createId = 'vertextypes-' + me.attr( 'uuid' ) + '-create';
 
     this.innerHTML =
       `
         <a href="#vertextypes/${me.attr( 'uuid' )}">${me.attr( 'name' )}</a>
         <a id="${deleteId}" href="#vertextypes">X</a>
+        <a id="${createId}" href="#vertextypes">+</a>
         <br>
       `;
 
@@ -21,6 +23,29 @@ class VertexTypeRow extends HTMLElement {
         $.ajax({
             url: '/barlomgdbconsolecontent/vertextypes/' + me.attr( 'uuid' ),
             type: 'DELETE',
+            success: function(result) {
+                // Do something with the result
+            }
+        })  .done(
+                    function () {
+                        riot.route( '/vertextypes', true );
+                    }
+            )
+            .fail(
+                    function ( jqxhr, textStatus, error ) {
+                        var err = textStatus + ", " + error;
+                        alert( "Request Failed: " + err );
+                    }
+            );
+    } );
+
+    $( `#${createId}` ).click( function() {
+        $.ajax({
+            url: '/barlomgdbconsolecontent/vertextypes/' + '6a0e5e00-ad1a-11e6-8cf0-080027832b30',
+            type: 'POST',
+            contentType: 'application/json',
+            data: `{ "uuid": "6a0e5e00-ad1a-11e6-8cf0-080027832b30", "name": "VertexTypeA" }`,
+            dataType: 'json',
             success: function(result) {
                 // Do something with the result
             }
