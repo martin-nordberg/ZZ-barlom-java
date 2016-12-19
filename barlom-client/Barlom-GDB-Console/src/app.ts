bf4e42aafed5d6fbe512a6c6ba51f026f9308126
Martin Nordberg <martin.nordberg@verizon.net>
2016-12-19 08:18:59 -0500

    Attempted to get cycle.js working with history (no joy using Typescript).

--- barlom-client/Barlom-GDB-Console/src/app.ts
+++ barlom-client/Barlom-GDB-Console/src/app.ts
@@ -1,55 +1,42 @@
-import {div, main, nav, VNode} from '@cycle/dom'
+import {div, main, VNode} from '@cycle/dom'
 import xs from 'xstream'
 import {DOMSource} from '@cycle/dom/xstream-typings'
 import {Stream} from 'xstream'
-import TopNavigation, {TopNavigationProps} from "./components/navigation/topnavigation";
+import TopNavigation, {IProps as ITopNavigationProps} from "./components/navigation/topnavigation";
 
-export type Sources = {
+export interface ISources {
     DOM : DOMSource
 }
 
-export type Sinks = {
+export interface ISinks {
     DOM : Stream<VNode>
 }
 
-export function App( sources : Sources ) : Sinks {
+export function App( sources : ISources ) : ISinks {
 
-    const topNavProps$ = xs.of<TopNavigationProps>( { title: "Barlom-GDB Console" } ).remember();
+    const topNavProps$ = xs.of<ITopNavigationProps>( { title: "Barlom-GDB Console" } ).remember();
 
     const topNav = TopNavigation( { DOM: sources.DOM, props$: topNavProps$ } );
 
-    const vtree$ = topNav.DOM.map(
-        topNavVTree =>
-            main(
-                '.o-grid.o-grid--no-gutter.o-panel', [
+    const vtree$ = topNav.DOM.map( topNavVTree =>
+        main( '.o-grid.o-grid--no-gutter.o-panel', [
 
-                    div(
-                        '.o-grid__cell--width-100.o-panel-container', [
+            div( '.o-grid__cell--width-100.o-panel-container', [
 
-                            topNavVTree,
+                topNavVTree,
 
-                            div(
-                                '.o-grid o-panel.o-panel--nav-top', [
-                                    div( '.o-grid__cell.o-grid__cell--width-30', { id: 'left-panel' }, ["Left Panel"] ),
-                                    div(
-                                        '.o-grid__cell.o-grid__cell--width-70',
-                                        { id: 'right-panel' },
-                                        ["Right Panel"]
-                                    )
-                                ]
-                            )
+                div( '.o-grid o-panel.o-panel--nav-top', [
+                    div( '.o-grid__cell.o-grid__cell--width-30', { id: 'left-panel' }, ["Left Panel"] ),
+                    div( '.o-grid__cell.o-grid__cell--width-70', { id: 'right-panel' }, ["Right Panel"] )
+                ] )
 
-                        ]
-                    )
+            ] )
 
-                ]
-            )
+        ] )
     );
 
-    const sinks = {
+    return {
         DOM: vtree$
     };
-
-    return sinks
 
 }
--- barlom-client/Barlom-GDB-Console/src/index.ts
+++ barlom-client/Barlom-GDB-Console/src/index.ts
@@ -1,12 +1,15 @@
 
+/// <reference path="./typings/history.d.ts"/>
+
 import {run} from '@cycle/xstream-run'
 import {makeDOMDriver} from '@cycle/dom'
-import {App} from './app'
-
-const main = App;
+import {App as main} from './app'
+import {makeHistoryDriver} from '@cycle/history'
+import {createHashHistory} from 'history';
 
 const drivers = {
-    DOM: makeDOMDriver( '#app' )
+    DOM: makeDOMDriver( '#app' ),
+    History: makeHistoryDriver( createHashHistory(), { capture: true } )
 };
 
 run( main, drivers );
