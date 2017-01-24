966163b0712092ff2971659bc8cc3ad17acb0615
Martin Nordberg <martin.nordberg@verizon.net>
2017-01-23 21:31:22 -0500

    Started babelify/ES6 project.

---
+++ barlom-client/barlom-gdbconsole/webapp/src/junk/stuff.js
@@ -0,0 +1,7 @@
+/**
+ * Created by mnordberg on 1/23/17.
+ */
+
+export const stuff = 42;
+
+export let moreStuff = () => 43;
---
+++ barlom-client/barlom-gdbconsole/webapp/src/main.js
@@ -0,0 +1,10 @@
+/**
+ * Barlom-GDB Console main program.
+ */
+
+import { stuff, moreStuff } from './junk/stuff.js'
+
+console.log( "Stuff: ", stuff );
+
+console.log( "More Stuff: ", moreStuff() );
+
