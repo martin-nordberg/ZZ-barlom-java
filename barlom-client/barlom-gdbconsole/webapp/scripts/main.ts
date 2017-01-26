0880a3c8ca7286c307f92821fed89a810b80bbfe
Martin Nordberg <martin.nordberg@verizon.net>
2017-01-25 21:40:15 -0500

    Made minor documentation improvements.

--- barlom-client/barlom-gdbconsole/webapp/scripts/main.ts
+++ barlom-client/barlom-gdbconsole/webapp/scripts/main.ts
@@ -5,9 +5,11 @@ import {classModule} from './lib/snabbdom/src/modules/class';
 import {propsModule} from './lib/snabbdom/src/modules/props';
 import {styleModule} from './lib/snabbdom/src/modules/style';
 import {eventListenersModule} from './lib/snabbdom/src/modules/eventlisteners';
+import {VNode} from "./lib/snabbdom/src/vnode";
 
 import counterList from './counterList';
 
+// Get a Snabbdom patch function with the normal HTML modules.
 const patch = snabbdomInit(
     [
         classModule,
@@ -17,11 +19,17 @@ const patch = snabbdomInit(
     ]
 );
 
+/**
+ * Runs one cycle of the Snabbdom event loop.
+ * @param state the latest model state.
+ * @param oldVnode the prior virtual DOM or the real DOM first time through.
+ * @param view the function to compute the new virtual DOM from the model.
+ * @param update the function to compute the new model state from the old state and a pending action.
+ */
+function main( state, oldVnode: VNode | Element, { view, update } ) {
 
-function main( state, oldVnode, { view, update } ) {
-
-    let eventHandler = e => {
-        const newState = update( state, e );
+    let eventHandler = action => {
+        const newState = update( state, action );
         main( newState, newVnode, { view, update } );
     };
 
@@ -31,12 +39,24 @@ function main( state, oldVnode, { view, update } ) {
 
 }
 
+/**
+ * Initializes the application state.
+ * @returns {{nextID: number, counters: Array}}
+ */
 function initState() {
-    return { nextID: 1, counters: [] };
+    return { nextID: 5, counters: [] };
 }
 
-main(
-    initState(),
-    document.getElementById( 'app' ),
-    counterList
-);
+// ---------------------------------------------------------------------------
+
+// Find the DOM node to drop our app in.
+let domNode = document.getElementById( 'app' );
+
+if ( domNode == null ) {
+    // Abandon hope if the real DOM node is not found.
+    console.log( "Cannot find application DOM node." );
+}
+else {
+    // Fire off the first loop of the lifecycle.
+    main( initState(), domNode, counterList );
+}
